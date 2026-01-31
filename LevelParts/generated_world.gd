340d8402fa456634d5a45cfd747323565466b476
Elias Brown <eli.marc.brown@gmail.com>
2026-01-31 16:07:10 +0100

    wip: level generation. Maze gen finished

--- LevelParts/generated_world.gd
+++ LevelParts/generated_world.gd
@@ -4,16 +4,13 @@ extends Node3D
 @export_group("Nodes")
 @export var floor_gridMap: GridMap
 @export var wallx_gridMap: GridMap
-@export var walldx_gridMap: GridMap
 @export var wallz_gridMap: GridMap
-@export var walldz_gridMap: GridMap
-
 
 const FLOOR_INDEX: int = 0
 const WALLX_INDEX: int = 1
-const WALLDX_INDEX: int = 2
-const WALLZ_INDEX: int = 3
-const WALLDZ_INDEX: int = 4
+const WALLZ_INDEX: int = 2
+const WALLX_DOOR_INDEX: int = 3
+const WALLZ_DOOR_INDEX: int = 4
 
 func spawn_floor(x: int, y: int)->void:
 	floor_gridMap.set_cell_item(Vector3i(x, 0, y), FLOOR_INDEX)	
@@ -22,9 +19,12 @@ func spawn_wall(x: int, y: int, direction: String):
 	var pos3 = Vector3i(x, 0, y)
 	if direction == "x":
 		wallx_gridMap.set_cell_item(pos3, WALLX_INDEX)
-	elif direction == "-x":
-		walldx_gridMap.set_cell_item(pos3, WALLDX_INDEX)
 	elif direction == "z":
-		wallz_gridMap.set_cell_item(pos3, WALLZ_INDEX)
-	elif direction == "-z":
-		walldz_gridMap.set_cell_item(pos3, WALLDZ_INDEX)
+		wallz_gridMap.set_cell_item(pos3, WALLZ_INDEX )
+
+func spawn_door(x: int, y: int, direction: String):
+	var pos3 = Vector3i(x, 0, y)
+	if direction == "x":
+		wallx_gridMap.set_cell_item(pos3, WALLX_DOOR_INDEX)
+	elif direction == "z":
+		wallz_gridMap.set_cell_item(pos3, WALLZ_DOOR_INDEX )
--- LevelParts/world.gd
+++ LevelParts/world.gd
@@ -5,18 +5,41 @@ extends Node3D
 
 # Generates a random level with the size determining the rows and columns size (identical)
 func generate_random_level(size: int) -> void:
+	randomize()
 	for x in range(size):
 		for y in range(size):
 			# generate a floor at the position
 			if gen_world:
 				gen_world.spawn_floor(x, y)
 
-			# generate each of the walls
-			gen_world.spawn_wall(x, y, "x")
-			gen_world.spawn_wall(x, y, "-x")
-			gen_world.spawn_wall(x, y, "z")
-			gen_world.spawn_wall(x, y, "-z")
-			
+			# always spawn walls enclosing the area
+			if x==0:
+				gen_world.spawn_wall(x-1, y, "x")
+			elif x==size-1:
+				gen_world.spawn_wall(x, y, "x")
+			if y==0:
+				gen_world.spawn_wall(x, y-1, "z")
+			elif y==size-1:
+				gen_world.spawn_wall(x, y, "z")
+
+			# Do binary tree maze generation
+			var can_south = x < size-1
+			var can_west = y < size-1
+
+			if can_south and can_west:
+				if randi() % 2:
+					gen_world.spawn_door(x, y, "x")
+					gen_world.spawn_wall(x, y, "z")
+				else:
+					gen_world.spawn_door(x, y, "z")
+					gen_world.spawn_wall(x, y, "x")
+			elif can_south:
+				gen_world.spawn_door(x, y, "x")
+				gen_world.spawn_wall(x, y, "z")
+			elif can_west:
+				gen_world.spawn_door(x, y, "z")
+				gen_world.spawn_wall(x, y, "x")
+
 			
 
 func _ready() -> void:
