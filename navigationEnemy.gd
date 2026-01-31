23779497288c193e2edb611a6656cde568c9d7e2
HodurGustavs <hodurgustavsson@gmail.com>
2026-01-31 16:25:25 +0100

    Enemy states now work

--- navigationEnemy.gd
+++ navigationEnemy.gd
@@ -5,13 +5,18 @@ extends CharacterBody3D
 @onready var detection_cone = $Detection
 @export var player: CharacterBody3D
 @export var speed = 2.0
+@export var hostile = true
+@export var wanderer = true
 @onready var movement_target_position: Vector3 = player.position
 enum states{idle, chasing, looking, wandering}
 var state = states.idle
-var direction = Vector3.FORWARD
+var time_to_wait = randf_range(5.0, 10.0)
+var wander = randi_range(0,1)
+var new_wander = true
+var direction = Vector3.DOWN
 func _ready():
-	nav_agent.path_desired_distance = 1.0
-	nav_agent.target_desired_distance = 0.5
+
+
 	
 	actor_setup.call_deferred()
 	
@@ -23,29 +28,88 @@ func actor_setup():
 func set_movement_target(movement_target: Vector3):
 	nav_agent.set_target_position(movement_target)
 	
+
+	
 func _physics_process(delta: float) -> void:
+	print_debug(states.keys()[state])
+	if state == states.idle and is_on_floor():
+		#print_debug("wait",time_to_wait)
+		time_to_wait = time_to_wait - delta
+		
+		if time_to_wait <= 0.0:
+
+			wander = randi_range(0, 1)
+			#print_debug(wander)
+			if wander == 1:
+				new_wander = true
+				state = states.wandering
+			elif wander == 0:
+				pass
+				
+			time_to_wait = randf_range(5.0, 10.0)
+		#print_debug(state)
+		
+		
 	
-	state = states.chasing
-	
+		
+	if state == states.wandering:
+		if !wanderer:
+			state = states.idle
+			return
+		if new_wander:
+			nav_agent.target_position = NavigationServer3D.map_get_random_point(nav_agent.get_navigation_map(),1,0)
+			new_wander = false
+		
+		
+		if nav_agent.distance_to_target() <= nav_agent.target_desired_distance:
+			
+			direction = Vector3.ZERO
+			state = states.idle
+		else:
+			direction = (nav_agent.get_next_path_position() - global_position).normalized()
+		#print_debug("wandering")
+		
+	if detection_cone.overlaps_body(player) and hostile:
+		LOS_ray.global_rotation = Vector3(0,0,0)
+		LOS_ray.set_target_position((player.global_position + Vector3(0,1,0)) - LOS_ray.global_position)
+		LOS_ray.force_raycast_update()
+		if !LOS_ray.is_colliding():
+			state = states.chasing
+		
+	if state == states.looking:
+		#nav_agent.target_position = nav_agent.get_final_position()
+		#print_debug(nav_agent.distance_to_target())
+		#print_debug(nav_agent.is_navigation_finished())
+		if nav_agent.distance_to_target() <= nav_agent.target_desired_distance:
+			#print_debug("Done looking")
+			direction = Vector3.ZERO
+			state = states.idle
+		else:
+			direction = (nav_agent.get_next_path_position() - global_position).normalized()
 	if state == states.chasing:
+		#print_debug("chasing")
 		LOS_ray.global_rotation = Vector3(0,0,0)
 		LOS_ray.set_target_position((player.global_position + Vector3(0,1,0)) - LOS_ray.global_position)
 		LOS_ray.force_raycast_update()
 		
-		if   !LOS_ray.is_colliding() :
-			print_debug(LOS_ray.get_collider())
+		if !LOS_ray.is_colliding() :
+			#print_debug(LOS_ray.get_collider())
+			
 			nav_agent.target_position = player.position
 			direction = (nav_agent.get_next_path_position() - global_position).normalized()
 			
-			velocity = direction * speed
-			look_at(player.position)
-			rotation.x = 0
-			rotation.z = 0
+			
 		else:
-			direction = Vector3.ZERO
-			velocity = direction * speed
+			state = states.looking
+		
+	velocity = direction * speed
+	#print_debug(direction)
+	look_at(nav_agent.get_final_position())
+	rotation.x = 0
+	rotation.z = 0
 	# Add the gravity.
 	if not is_on_floor():
+		
 		velocity += get_gravity() * delta
 
 	
