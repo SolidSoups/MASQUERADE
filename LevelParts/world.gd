8f58bee0c34e389ad207b5161469da2cc8fe4251
Elias Brown <eli.marc.brown@gmail.com>
2026-01-31 13:24:37 +0100

    wip: basic wall generation

--- LevelParts/world.gd
+++ LevelParts/world.gd
@@ -1,29 +1,38 @@
 extends Node3D
 
-var level_part = preload("res://LevelParts/room_template.tscn")
-var level_parts: Array[Node3D] = []
+@export_group("Parts")
+@export var fountain_part: PackedScene
+@export var wall_part: PackedScene
+@export var doorwall_part: PackedScene
 
-func clear_parts() -> void:
-	# Queue all parts for removal
-	for part in level_parts:
-		part.queue_free()
-	level_parts.clear()
+var wall_positions: Array[Vector3] = [
+	Vector3(7.5, 0, 0), 
+	Vector3(-7.5, 0, 0), 
+	Vector3(0, 0, 7.5), 
+	Vector3(0, 0, -7.5)
+]
+var wall_yrotations: Array[float] = [90.0, 90.0, 0, 0]
 
-func spawn_box(pos: Vector3)-> void:
-	var box = level_part.instantiate()
-	add_child(box)
-	box.global_position = pos
-	level_parts.append(box)
+# spawn a level surrounded by walls
+func spawn_level(level: PackedScene):
+	var spwnLvl = level.instantiate()	
+	add_child(spwnLvl)
+	spwnLvl.global_position = Vector3(0, 0, 0)
 
-func spawn_rubix() -> void:
-	const y = 1
-	for x in range(3):
-			for z in range(3):
-				spawn_box(Vector3(x, y, z))
+	# Spawn walls all around	
+	for r in range(4):
+		var newWall: Node3D
+		if randf() < 0.3:
+			newWall = doorwall_part.instantiate()
+		else:
+			newWall = wall_part.instantiate()
+		spwnLvl.add_child(newWall)
+		newWall.global_position = wall_positions[r]
+		newWall.rotation_degrees = Vector3(0, wall_yrotations[r], 0)
+
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventKey:
 		if event.pressed and event.keycode == KEY_F:
-			clear_parts()
-			spawn_rubix()
+			spawn_level(fountain_part)
 
